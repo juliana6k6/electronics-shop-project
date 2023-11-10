f13d2bfc035bdaa5a1d41ae7dc7c133ba3331a48
Juliana <juliana6k@mail.ru>
2023-11-11 01:09:32 +0300

    HW4.4

--- src/item.py
+++ src/item.py
@@ -28,6 +28,15 @@ class Item:
     def __str__(self):
         return f'{self.__name}'
 
+    def __add__(self, other):
+        """
+        Рeализует возможность сложения экземпляров класса `Phone` и `Item` по количеству товара в магазине,
+        при этом нельзя складывать `Phone` или `Item` с экземплярами не `Phone` или `Item` классов
+        """
+        if isinstance(other, Item):
+            return int(self.quantity) + int(other.quantity)
+        else:
+            raise TypeError("Нельзя сложить классы 'Item' и чем-то другим.")
 
     @property
     def name(self):
