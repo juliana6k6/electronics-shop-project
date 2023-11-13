39c1d6968c2f668e1b13fed875df1c5c762de625
Juliana <juliana6k@mail.ru>
2023-11-14 02:14:01 +0300

    HW5ю1

---
+++ src/keyboard.py
@@ -0,0 +1,23 @@
+from src.item import Item
+
+
+class MixinLang:
+    """Для расширения функционала Keyboard"""
+    __language = 'EN'
+
+    @property
+    def language(self):
+        return self.__language
+
+    def change_lang(self):
+        """Изменяет язык в клавиатуре"""
+        if self.__language == 'EN':
+            self.__language = 'RU'
+        else:
+            self.__language = 'EN'
+
+
+class Keyboard(Item, MixinLang):
+
+    def __init__(self, name: str, price: float, quantity: int):
+        super().__init__(name, price, quantity)
